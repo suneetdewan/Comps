65483a9d6bf54d7dcb0fd06926d804b056ac6eac
suneetdewan <suneet.dewan@gmail.com>
2016-03-03 16:50:43 -0600

    Regression run on the data where all of the non-responses were removed

---
+++ Regressions_main.R
@@ -0,0 +1,98 @@
+## Regression on dataset that removes all non-response
+
+WaveB <- read.csv("clean_npremoved.csv")
+
+# Remove the observations with empty Education
+WaveB <- WaveB[-375,]
+which(! complete.cases(WaveB))
+
+# Remove the observations that have missing values
+WaveB <- WaveB[-c(65, 101, 190, 269, 427, 571, 713, 752), ]
+
+WaveB$code <- factor(WaveB$industry_code)
+WaveB$education_smaller <- factor(WaveB$education_smaller, levels = c("High School or Below", "Some college", "Bachelors degree", "Graduate degree"))
+
+
+
+# SURVIVAL
+
+# Only Control
+survive3_logit_0 <- glm(survive_3 ~ white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(survive3_logit_0)
+
+# General + Control
+survive3_logit_1 <- glm(survive_3 ~ education_smaller + work_experience + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(survive3_logit_1)
+
+# General + Industry + Control
+survive3_logit_2 <- glm(survive_3 ~ education_smaller + work_experience+ industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(survive3_logit_2)
+
+# General + Entrepreneurial + Control
+survive3_logit_3 <- glm(survive_3 ~ education_smaller + work_experience + manager_exp + entrep_exp + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(survive3_logit_3)
+
+#everything
+survive3_logit <- glm(survive_3 ~ education_smaller + work_experience + manager_exp + entrep_exp + industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(survive3_logit)
+
+
+anova(survive3_logit_0, survive3_logit, test = "Chisq")
+
+
+
+#PROFIT
+
+# Only Control
+profit3_logit_0 <- glm(profit ~ white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(profit3_logit_0)
+
+# General + Control
+profit3_logit_1 <- glm(profit ~ education_smaller + work_experience + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(profit3_logit_1)
+
+# General + Industry + Control
+profit3_logit_2 <- glm(profit ~ education_smaller + work_experience+ industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(profit3_logit_2)
+
+# General + Entrepreneurial + Control
+profit3_logit_3 <- glm(profit ~ education_smaller + work_experience + manager_exp + entrep_exp + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(profit3_logit_3)
+
+#everything
+profit3_logit <- glm(profit ~ education_smaller + work_experience + manager_exp + entrep_exp + industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(profit3_logit)
+
+anova(profit3_logit_0, profit3_logit, test = "Chisq")
+
+
+
+# REVENUE
+
+# Only Control
+rev3_logit_0 <- glm(rev ~ white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(rev3_logit_0)
+
+# General + Control
+rev3_logit_1 <- glm(rev ~ education_smaller + work_experience + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(rev3_logit_1)
+
+# General + Industry + Control
+rev3_logit_2 <- glm(rev ~ education_smaller + work_experience+ industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(rev3_logit_2)
+
+# General + Entrepreneurial + Control
+rev3_logit_3 <- glm(rev ~ education_smaller + work_experience + manager_exp + entrep_exp + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight, na.action = na.exclude)
+summary(rev3_logit_3)
+
+#everything
+rev3_logit <- glm(rev ~ education_smaller + work_experience + manager_exp + entrep_exp + industry_exp + major_reason + white + parents + Gender + alone_or_not + net_worth:net_worth_dummy + code + X35hours + REGIONA, data = WaveB, family = 'binomial', weights = WaveB$Weight)
+summary(survive3_logit)
+
+anova(rev3_logit_0, rev3_logit, test = "Chisq")
+
+
+
+# Checking multi-collinearity
+library(car)
+vif(survive3_logit)
